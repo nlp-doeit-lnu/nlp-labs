e57c6be6ef0890ed2da8ca3355810b00267cfc87
Bohdan Horon <goronbohdan@gmail.com>
2024-09-03 11:43:25 +0300

    reogranize programs/jars folder

--- programs/jars/++repetition3.3jar/Executable_2023_big4GB.bat
+++
@@ -1,2 +0,0 @@
-java -Xmx4G --module-path "javafx-sdk-18.0.1\lib" --add-modules javafx.controls,javafx.fxml -jar projFedorova3_2023.jar
-pause
--- scripts/repetition_parameter.sh
+++ scripts/repetition_parameter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /opt/jdk-18/bin/java -Xmx4G --module-path "/opt/javafx-sdk-17.0.12/lib" --add-modules javafx.controls,javafx.fxml -jar /mnt/jars/++repetition3.3jar/projFedorova3_2023.jar
+exec /opt/jdk-18/bin/java -Xmx4G --module-path "/opt/javafx-sdk-17.0.12/lib" --add-modules javafx.controls,javafx.fxml -jar /mnt/jars/projFedorova3_2023.jar
