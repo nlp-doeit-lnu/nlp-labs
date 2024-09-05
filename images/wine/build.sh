c928411fa8ea81a2429deb08511aee3fe80fb955
Bohdan Horon <goronbohdan@gmail.com>
2024-09-05 13:19:05 +0300

    fix wine build process

--- images/wine/build.sh
+++ images/wine/build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-docker build -t nlp-wine .
+NLP_HOME=$PWD
+
+NLP_WINE=$NLP_HOME/images/wine
+
+IMAGE=nlp-wine
+
+if [ -z "$(docker images | grep $IMAGE)" ]
+then
+	docker build -t $IMAGE $NLP_WINE
+fi
+
+read -p "Введіть шлях до папки з даними: " NLP_DATA
+
+read -p "Введіть шлях до папки, в яку ви хотіли б зберігати дані: " NLP_RESULTS
+
+echo "створюємо скрипт $NLP_WINE/run.sh"
+
+sed -e "s@%NLP_HOME%@$NLP_HOME@g" \
+    -e "s@%NLP_DATA%@$NLP_DATA@g" \
+    -e "s@%NLP_RESULTS%@$NLP_RESULTS@g" \
+    $NLP_WINE/run.template > $NLP_WINE/run.sh
